c7faebedee2e679c079e43509470c5646b619dbc
Joshua Luppes <cooljoebob64@gmail.com>
2020-12-07 15:15:40 -0600

    Updated formatting

--- js/Ass3.js
+++ js/Ass3.js
@@ -3,32 +3,29 @@ function inventoryList() {
   // write your code here
   let theList = [];
   let bigList = {
-        
-        add : name => {
-            if(!theList.includes(name)){
-                theList.push(name);
-            } else {
-                console.log("Already an entry on the list named: " + name);
-            }
-        },
+    add: (name) => {
+      if (!theList.includes(name)) {
+        theList.push(name);
+      } else console.log("Already an entry on the list named: " + name);
+    },
 
-        remove : name => {
-            if(theList.includes(name)){
-                theList.splice(theList.indexOf(name),1);    
-            } else console.log("No entry on list by the name of: " + name);
-        },
-                    
-        getList : () => {
-            return theList;
-        },   
-    }
-    return bigList;
+    remove: (name) => {
+      if (theList.includes(name)) {
+        theList.splice(theList.indexOf(name), 1);
+      } else console.log("No entry on list by the name of: " + name);
+    },
+
+    getList: () => {
+      return theList;
+    },
+  };
+  return bigList;
 }
 
 // Test it out!! Everything below this line is just for testing. Console is logged after each step.
 
 // Make a new list, and see what's in it
-let SantasList = new inventoryList;
+let SantasList = new inventoryList();
 console.log(SantasList.getList());
 
 // Add an item
